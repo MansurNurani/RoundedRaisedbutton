17dfd206f651f70df2a985e6471430c327db10a2
MansurNurani <mansur.nurani@gmail.com>
2022-05-19 11:34:15 +0430

    default

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ const App = () => {
         <MenuIcon color="primary" className={classes.largeIcon} />
       </Button>
 
-      <Box sx={{ borderRadius: "50%" }}>ddd1</Box>
+      <Box sx={{ borderRadius: "50%" }}>ddd12</Box>
     </>
   );
 };
