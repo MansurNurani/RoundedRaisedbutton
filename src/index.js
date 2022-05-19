ed68642db97ec51c24b2e4ad8d3a33ace859642d
MansurNurani <31807132+MansurNurani@users.noreply.github.com>
2022-05-19 11:26:41 +0430

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { render } from "react-dom";
+import { makeStyles } from "@mui/styles";
+//import Button from "@material-ui/core/Button";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+import MenuIcon from "@mui/icons-material/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  largeIcon: {
+    width: "50px",
+    height: "50px"
+  },
+  RoundedButton: {
+    width: "170px",
+    height: "170px",
+    borderRadius: "50%",
+    backgroundColor: "transparent"
+  }
+}));
+const App = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Button
+        RoundedButton
+        variant="primary"
+        color="primary"
+        className={classes.RoundedButton}
+      >
+        <MenuIcon color="primary" className={classes.largeIcon} />
+      </Button>
+
+      <Box sx={{ borderRadius: "50%" }}>سلام</Box>
+    </>
+  );
+};
+
+render(<App />, document.getElementById("root"));
