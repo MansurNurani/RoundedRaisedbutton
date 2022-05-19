d3900a5735cf2df6936c4644692b50d3c0f962b8
MansurNurani <31807132+MansurNurani@users.noreply.github.com>
2022-05-19 15:50:59 +0430

    a

--- src/index.js
+++ src/index.js
@@ -2,32 +2,54 @@ import React from "react";
 import { render } from "react-dom";
 //import Button from "@material-ui/core/Button";
 import Button from "@mui/material/Button";
+import Drawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
 
 import MenuIcon from "@mui/icons-material/Menu";
 
 const App = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOnClose = () => {
+    setOpen(false);
+  };
+  const handleButtonClick = () => {
+    setOpen(true);
+  };
   return (
     <>
       <Button
+        onClick={handleButtonClick}
         sx={{
-          width: "170px",
-          height: "170px",
-          borderRadius: "50%",
-          backgroundColor: "transparent"
+          width: "270px",
+          height: "270px",
+          borderRadius: "50% !important"
         }}
-        RoundedButton
-        variant="primary"
-        color="primary"
+        color="success"
+        variant="contained"
       >
-        <MenuIcon color="primary" sx={{ width: "50px", height: "50px" }} />
+        <MenuIcon
+          sx={{
+            width: "90% !important",
+            height: "90% !important"
+          }}
+        />
       </Button>
-
-      <Box sx={{ borderRadius: "50%" }}>aaa</Box>
-      <Box sx={{ borderRadius: "50%" }}>aaa</Box>
-      <Box sx={{ borderRadius: "50%" }}>aaa</Box>
-      <Box sx={{ borderRadius: "50%" }}>aaa</Box>
-      <Box sx={{ borderRadius: "50%" }}>aaa</Box>
+      <Drawer
+        transitionDuration={{ enter: 400, exit: 400 }}
+        anchor="right"
+        open={open}
+        onClose={handleOnClose}
+      >
+        {
+          <Box sx={{ width: "300px", padding: "20px" }}>
+            Salam1
+            <hr />
+            Salam2
+            <hr />
+            Salam3
+          </Box>
+        }
+      </Drawer>
     </>
   );
 };
